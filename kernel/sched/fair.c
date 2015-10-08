ee0823a0eefa89d0f10d87fe184fec9dfc898e53
jollaman999 <admin@jollaman999.com>
2015-10-08 12:32:04 +0530

    sched: Set sched_upmigrate_min_nice to 9
    Android background processes are set to nice 10. Never schedule these on the a57s.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1486,8 +1486,8 @@ unsigned int __read_mostly sysctl_sched_downmigrate_pct = 60;
  * Tasks whose nice value is > sysctl_sched_upmigrate_min_nice are never
  * considered as "big" tasks.
  */
-static int __read_mostly sched_upmigrate_min_nice = 15;
-int __read_mostly sysctl_sched_upmigrate_min_nice = 15;
+static int __read_mostly sched_upmigrate_min_nice = 9;
+int __read_mostly sysctl_sched_upmigrate_min_nice = 9;
 
 /* grp upmigrate/downmigrate */
 unsigned int __read_mostly sched_grp_upmigrate;
