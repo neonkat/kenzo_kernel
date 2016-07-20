37f8e9676ae17f723f05d02289aa2beaca1d6951
neonkat <neonkatxda@gmail.com>
2016-07-20 15:01:34 +0530

    Revert "add yankdemand"
    This reverts commit 9cbba57b1bc5113f5975844ce7a1b52e3ab90eeb.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -438,9 +438,6 @@ extern struct cpufreq_governor cpufreq_gov_elementalx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
-extern struct cpufreq_governor cpufreq_gov_yankdemand;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
 #endif
 
 /*********************************************************************
