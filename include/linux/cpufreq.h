d91da7ce8c273db1b11af1c47c40593c74ad7d97
neonkat <neonkatxda@gmail.com>
2016-08-24 14:48:28 +0530

    add some darkness(thanks @alucard24)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -446,6 +446,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
 extern struct cpufreq_governor cpufreq_gov_despair;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_despair)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 /*********************************************************************
