5b3e99e64e4ccfbcb034ceebcc1ad289e018ce00
neonkat <neonkatxda@gmail.com>
2017-03-22 02:06:24 +0530

    Add in blu_active governer by @engstk

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -467,6 +467,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 /*********************************************************************
