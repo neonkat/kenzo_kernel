c8aba547dc4a6c91ab54dd5bf4637421109dd749
neonkat <neonkatxda@gmail.com>
2017-03-29 02:01:08 +0530

    ZEN: Update to zen-tune v4.10
    https://github.com/zen-kernel/zen-kernel/commit/42857f172a22b40ac21807af2b329a1d14897fc6

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -92,7 +92,7 @@ int vm_highmem_is_dirtyable;
  * The generator of dirty data starts writeback at this percentage
  */
 #ifdef CONFIG_ZEN_INTERACTIVE
-int vm_dirty_ratio = 30;
+int vm_dirty_ratio = 50;
 #else
 int vm_dirty_ratio = 20;
 #endif
