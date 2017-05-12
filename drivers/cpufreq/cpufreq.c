a9b25452c3a82c69ddbccffd83884d86d33b1ac5
cb <billchen1977@gmail.com>
2017-05-12 10:54:56 +0800

    CPU voltage control for kenzo
    Change-Id: Ic6774e8c80f7d8181c4b0028dc957eddb38b6143

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -28,6 +28,7 @@
 #include <linux/slab.h>
 #include <linux/syscore_ops.h>
 #include <linux/tick.h>
+#include <linux/pm_opp.h>
 #include <trace/events/power.h>
 
 /* HACK: Prevent big cluster turned off when changing governor settings. */
@@ -681,19 +682,21 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
-extern ssize_t get_Voltages(char *buf);
+#ifdef CONFIG_MACH_XIAOMI_KENZO
+extern ssize_t cpu_clock_get_vdd(char *buf);
+extern ssize_t cpu_clock_set_vdd(const char *buf, size_t count);
+
 static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf)
 {
-	return get_Voltages(buf);
+	return cpu_clock_get_vdd(buf);
 }
 
-extern ssize_t set_Voltages(char *buf);
-static ssize_t store_UV_mV_table(struct cpufreq_policy *policy, const char *buf, size_t count)
+static ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
+	const char *buf, size_t count)
 {
-	set_Voltages(buf);
-	return count;
+	return cpu_clock_set_vdd(buf, count);
 }
-
+#endif
 
 cpufreq_freq_attr_ro_perm(cpuinfo_cur_freq, 0400);
 cpufreq_freq_attr_ro(cpuinfo_min_freq);
@@ -709,7 +712,9 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+#ifdef CONFIG_MACH_XIAOMI_KENZO
 cpufreq_freq_attr_rw(UV_mV_table);
+#endif
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -723,6 +728,9 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+#ifdef CONFIG_MACH_XIAOMI_KENZO
+	&UV_mV_table.attr,
+#endif
 	NULL
 };
 
